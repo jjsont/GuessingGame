3cb19c4303350b1ae15f6ad93c21ccc712753881
BT_1N3_05@BKTECH.LOCAL <BT_1N3_05@BKTECH.LOCAL>
2018-10-10 14:48:12 -0400

    sssssssssssssss

---
+++ src/GuessingGame.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class GuessingGame {
+    public static void randomNumber(double num) {
+        Scanner input= new Scanner(System.in);
+        System.out.println("What's your name?");
+        String name= input.nextLine();
+        double random= Math.random();
+        int x= (int)x*5+1);
+        System.out.println("What is the random number from 1-5, "+name+"?");
+         x = input.nextLine();
+        while (!x = num)
+        {
+            System.out.println("You got it wrong, "+name+" Guess again");
+            x = input.nextLine();
+}
