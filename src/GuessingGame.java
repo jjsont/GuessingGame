6a61c1c4e8de3ff5154f109990f96bd11792bafe
JaSON <jtan5662@bths.edu>
2018-10-11 14:47:09 -0400

    go home today ur beat

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 public class GuessingGame {
     public static void main (String[] args) {
         Scanner input = new Scanner(System.in);
+        int wrong = 0;
         System.out.println("What's your name?");
         String name = input.nextLine();
         int random = (int)(Math.random()*11);
@@ -10,7 +11,11 @@ public class GuessingGame {
         while (random != num) {
             System.out.println("You got it wrong, " + name + " guess again");
             num = input.nextInt();
+            if(random != num) {
+                wrong ++;
+            }
         }
         System.out.println("Good job, " + name +" you finally got it");
+        System.out.println("Total attempts:" + wrong);
     }
 }
