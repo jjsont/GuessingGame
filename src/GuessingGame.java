b0e3e3a0aadc9b23bb773e13e9534fcb713c8566
JaSON <jtan5662@bths.edu>
2018-10-11 14:31:18 -0400

    go home today ur beat

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -1,15 +1,16 @@
 import java.util.Scanner;
 public class GuessingGame {
-    public static void randomNumber(double num) {
-        Scanner input= new Scanner(System.in);
+    public static void main (String[] args) {
+        Scanner input = new Scanner(System.in);
         System.out.println("What's your name?");
-        String name= input.nextLine();
-        double random= Math.random();
-        int x= (int)x*5+1);
-        System.out.println("What is the random number from 1-5, "+name+"?");
-         x = input.nextLine();
-        while (!x = num)
-        {
-            System.out.println("You got it wrong, "+name+" Guess again");
-            x = input.nextLine();
+        String name = input.nextLine();
+        int random = (int)(Math.random()*11);
+        System.out.println("What is the random number from 1-10, " + name + "?");
+        int num = input.nextInt();
+        while (random != num) {
+            System.out.println("You got it wrong, " + name + " guess again");
+            num = input.nextInt();
+        }
+        System.out.println("Good job, " + name +" you finally got it");
+    }
 }
