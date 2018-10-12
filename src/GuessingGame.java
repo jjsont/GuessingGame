4a83d0299cbd860e50d6f92e9281ab148692f4fd
Jason <jtan5662@bths.edu>
2018-10-12 15:30:59 -0400

    go to practice?

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -6,7 +6,7 @@ public class GuessingGame {
         System.out.println("What's your name?");
         String name = input.nextLine();
         int random = (int) (Math.random() * 11);
-        System.out.println("What is the random number from 0-10, " + name + "?");
+        System.out.println("What is the random number from 1-10, " + name + "?");
         int num = input.nextInt();
         while (random != num) {
             System.out.println("You got it wrong, " + name + " guess again");
