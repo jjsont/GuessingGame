64ccce1d16f5d09022e75e61cb6f5d3dc013d077
Jason <jtan5662@bths.edu>
2018-10-12 15:22:08 -0400

    go to practice?

--- src/UserGuess.java
+++ src/UserGuess.java
@@ -1,14 +1,28 @@
 import java.util.Scanner;
 public class UserGuess {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int aiGuess = (int)(Math.random()*101)
+        int min= 1;
+        int max= 101;
+        int guess = 50;
         System.out.println("Hello, my name is AI. AI wonder what's your number?");
-        System.out.println("Hmmmm...");
-        while (true) {
-            if (aiGuess < 5) {
-                break;
+        System.out.println("Please type higher, lower, or correct after my guess");
+        System.out.println("Hmmm... Your number is " +guess);
+        String pewpew = "";
+        while (!(pewpew.equals("correct"))) {
+            pewpew = input.nextLine();
+            if (pewpew.equals("higher")) {
+                min = guess;
+                guess = (min+max)/2;
+                System.out.println("Your number is " +guess);
             }
+            if (pewpew.equals("lower")) {
+                max = guess;
+                guess = (min+max)/2;
+                System.out.println("Your number is " +guess);
+            }
+            }
+        System.out.println("You've guessed it! My number is " + guess);
         }
+    }
 
-}
